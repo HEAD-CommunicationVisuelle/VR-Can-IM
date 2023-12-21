0d29b4d29683432829d3f16614ffa17e8595a1a1
Irène <>
2023-12-21 11:23:03 +0100

    initial commit

---
+++ Assets/Exercices/Scripts/ExBounceSound.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Play a sound when a collision in detected
+public class ExBounceSound : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    // Detect collision
+    void OnCollisionEnter(Collision collision)
+    {
+        //Play a sound
+        GetComponent<AudioSource>().Play();
+
+    }
+}
